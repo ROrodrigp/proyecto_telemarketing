b7319bd117f0a31ed80f3913db6a739acce44c93
rodrigog <rodrigo.ga.garcia@oracle.com>
2023-03-29 10:38:52 -0600

    Anadiendo script para la carga ordenada de daots

---
+++ orden-carga-datos.sql
@@ -0,0 +1,7 @@
+--Ejecutar carga de datos en el orden correcto 
+@insertar_PERSONA.sql
+@insertar_MEMBRESIA.sql
+@insertar_CLIENTE.sql
+@insertar_PROVEEDOR.sql
+@insertar_EMPLEADO.sql
+@insertar_MARCA.sql
