7dcf1fe9cc8f9536724966c2fca052d131922e19
rodrigog <rodrigo.ga.garcia@oracle.com>
2023-03-31 11:42:24 -0600

    Corrigiendo privilegios necesarios para el usuario

--- crear_usuario_privilegios.sql
+++ crear_usuario_privilegios.sql
@@ -1,7 +1,8 @@
 
 alter session set container=rgpdip_pdb1;
 CREATE USER rodrigo05 IDENTIFIED BY D1Dd1c913a46485946bd## DEFAULT TABLESPACE users QUOTA UNLIMITED ON users;
-grant create session, create table, create trigger, create procedure, CREATE ANY DIRECTORY, EXECUTE ON DBMS_LOB to rodrigo05;
+grant create session, create table, create trigger, create procedure, CREATE ANY DIRECTORY to rodrigo05;
+grant EXECUTE ON DBMS_LOB to rodrigo05;
 commit;
 
 
