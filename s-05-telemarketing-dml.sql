38b2e168e1b6fca453a8a82908d6b116315e961f
rodrigog <rodrigo.ga.garcia@oracle.com>
2023-05-19 23:34:30 -0600

    Trayendo datos para tabla venta y venta detalle

---
+++ s-05-telemarketing-dml.sql
@@ -0,0 +1,9 @@
+set feedback off
+@insertar_PERSONA.sql
+@insertar_MEMBRESIA.sql
+@insertar_CLIENTE.sql
+@insertar_PROVEEDOR.sql
+@insertar_EMPLEADO.sql
+@insertar_DESCUENTO.sql
+@insertar_MARCA.sql
+set feedback on
